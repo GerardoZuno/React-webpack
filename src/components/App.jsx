9e7024e8d01e17cb2f606046826e3b2e2fb335fb
Gerardo Zuno <gerardozunounitec@gmail.com>
2021-04-06 18:18:36 -0500

    Add Babel

---
+++ src/components/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const App = () => {
+    console.log('Helo')
+}
+
+export default App 
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
+
+//sirve para mostrar en un archivo de html nuestra app
+ReactDOM.render(<App />, document.getElementById('app')); 
+
+
