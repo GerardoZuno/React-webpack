7746b291f35f70f210c0a3ab14da59ee83ea0107
Gerardo Zuno <gerardozunounitec@gmail.com>
2021-04-06 19:29:15 -0500

    Add Css/sass

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
+import '../src/styles/global.scss'
 
 //sirve para mostrar en un archivo de html nuestra app
 ReactDOM.render(<App />, document.getElementById('app')); 
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 
 module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
+        filename: 'main.js',
     },
     resolve: {
         extensions: ['.js', '.jsx']  
@@ -27,13 +28,24 @@ module.exports = {
                         loader: 'html-loader'
                     }
                 ]
-              }
+              },
+              {
+                test: /\.s[ac]ss$/,
+                use: [
+                      'style-loader',
+                      'css-loader',
+                      'sass-loader',
+                    ]
+              }  
         ]       
     },
     plugins: [
        new HtmlWebPackPlugin({         
                template: './public/index.html',
                filename: './index.html'           
+       }),
+       new MiniCssExtractPlugin({
+           filename: '[name].css'
        })
     ],
     devServer: {
